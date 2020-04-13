3c74644d5dea8487a082b466d930bdbcd58b5cb2
gpmontt <giovanni.perez-montt@alumni.fh-aachen.de>
2020-04-13 16:27:10 +0200

    change format release from gz to xz

--- fetch-pandoc.sh
+++ fetch-pandoc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -eux
 echo 'test'
 PANDOC_VERSION=$1
-# DEST=${2}-pandoc.deb
+ DEST=${2-pandoc.deb}
 echo $PANDOC_VERSION
-#URL=https://github.com/jgm/pandoc/releases/download/${PANDOC_VERSION}/pandoc-${PANDOC_VERSION}-1-amd64.deb
-#exec wget --continue --output-document ${DEST} ${URL}
+URL=https://github.com/jgm/pandoc/releases/download/${PANDOC_VERSION}/pandoc-${PANDOC_VERSION}-1-amd64.deb
+echo $URL
+exec wget --continue --output-document ${DEST} ${URL}
